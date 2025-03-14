ad7ae50d9b8eeb8483b7e7b0d9ad789fcc4b5aa2
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-03-13 17:36:34 -0700

    layout

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,19 +12,29 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using xBindDataExample.Models;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace DataBindingListGrid040
+namespace xBindDataExample
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<Book> Books;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Books = BookManager.GetBooks();
+        }
+
+        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var book = (Book)e.ClickedItem;
+            ResultTextBlock.Text = "You selected " + book.Title;
         }
     }
 }
